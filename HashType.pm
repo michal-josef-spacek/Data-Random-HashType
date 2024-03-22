62542318bf5730f7bf5c705ecf6918d117fdf81f
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-03-22 16:02:44 +0100

    Regen documentation and example

--- HashType.pm
+++ HashType.pm
@@ -222,28 +222,34 @@ Returns instance of L<Data::HashType>.
  # [
  #     [0] Data::HashType  {
  #             parents: Mo::Object
- #             public methods (5):
+ #             public methods (7):
  #                 BUILD
+ #                 Error::Pure:
+ #                     err
  #                 Mo::utils:
- #                     check_bool, check_length, check_number, check_required
+ #                     check_bool, check_isa, check_length, check_number, check_required
  #             private methods (0)
  #             internals: {
- #                 active   1,
- #                 id       1,
- #                 name     "SHA-256"
+ #                 active       1,
+ #                 id           1,
+ #                 name         "SHA-256",
+ #                 valid_from   2023-04-04T00:00:00 (DateTime)
  #             }
  #         },
  #     [1] Data::HashType  {
  #             parents: Mo::Object
- #             public methods (5):
+ #             public methods (7):
  #                 BUILD
+ #                 Error::Pure:
+ #                     err
  #                 Mo::utils:
- #                     check_bool, check_length, check_number, check_required
+ #                     check_bool, check_isa, check_length, check_number, check_required
  #             private methods (0)
  #             internals: {
- #                 active   1,
- #                 id       2,
- #                 name     "SHA-384"
+ #                 active       1,
+ #                 id           2,
+ #                 name         "SHA-384",
+ #                 valid_from   2023-09-03T00:00:00 (DateTime)
  #             }
  #         }
  # ]
